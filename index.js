13de2fa6a0c23942ea8622ca3a9dfb50b3641f90
Pooja Deglure <105205600+Poovt@users.noreply.github.com>
2023-03-01 12:03:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function mySubmit(){
+    let exampleInputName= document.querySelector("#exampleInputName")
+
+    let Name=document.querySelector("#Name")
+    Name.innerHTML= "welcome " + exampleInputName.value;
+
+    // let exampleInputEmail1=document.querySelector("#exampleInputEmail1")
+    // let exampleInputContact=document.querySelector("#exampleInputContact")
+    // let exampleInputLocation=document.querySelector("#exampleInputLocation")
+    // let exampleInputEdu=document.querySelector("#exampleInputEdu")
+    // let exampleInputDesg=document.querySelector("#exampleInputDesg")
+
+   
+}
